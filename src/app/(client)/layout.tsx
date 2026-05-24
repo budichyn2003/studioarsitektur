f4dc0f30cdeeab6da60ab337a3e67640b0ba4b13
budichyn2003 <budigubernur15@gmail.com>
2026-05-24 23:58:48 +0700

    mobile friendly resmi done

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -8,7 +8,11 @@ export default function ClientLayout({
   return (
     <div className="flex w-full min-h-screen bg-white">
       <Sidebar />
-      <main className="ml-[300px] w-[calc(100%-300px)] min-h-screen relative flex flex-col">
+      {/* PERBAIKAN RESPONSIVE DI SINI:
+        - Mobile: w-full dan pt-[80px] (agar tidak tertutup burger header)
+        - Desktop (lg): ml-[300px], w-[calc(100%-300px)], dan pt-0 (kembali ke desain asli)
+      */}
+      <main className="w-full pt-[80px] lg:pt-0 lg:ml-[300px] lg:w-[calc(100%-300px)] min-h-screen relative flex flex-col">
         {children}
       </main>
     </div>
