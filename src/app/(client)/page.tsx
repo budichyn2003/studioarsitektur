2f94bc02d5cb43903f93254876e6d9a183ba02ab
budichyn2003 <budigubernur15@gmail.com>
2026-05-26 19:15:29 +0700

    done

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -33,25 +33,25 @@ export default function HomePage() {
   }, [images, delay]);
 
   if (images.length === 0) {
-    return (
-      <div className="absolute inset-0 w-full h-full overflow-hidden bg-white">
-        <div className="absolute left-1/2 top-2/3 -translate-x-1/2 -translate-y-1/2 text-[700px] lg:text-[900px] font-bold text-[#F9F9F9] select-none z-0 leading-none tracking-tighter animate-pulse">N</div>
-      </div>
-    );
+    return <div className="absolute inset-0 w-full h-full bg-white"></div>;
   }
 
   return (
     <div className="absolute inset-0 w-full h-full overflow-hidden bg-white">
       
-      {/* WATERMARK "N" - TIDAK DIUBAH */}
+      {/* 🛠️ PANDUAN MENGGESER WATERMARK 🛠️
+        - Geser Kanan/Kiri : Ubah angka "left-[55%]" (HP) dan "lg:left-[42%]" (Laptop). Makin besar angkanya, makin ke kanan.
+        - Geser Atas/Bawah : Ubah angka "bottom-[0px]" (HP) dan "lg:bottom-[20px]" (Laptop). Makin besar angkanya (plus), makin naik ke atas.
+      */}
       <div 
-        className="absolute left-[50%] lg:left-[35%] bottom-[-40px] lg:bottom-[-70px] xl:bottom-[-90px] -translate-x-1/2 text-[250px] lg:text-[450px] xl:text-[550px] font-bold text-[#F9F9F9] select-none z-0 leading-none tracking-tighter"
+        className="absolute left-[55%] lg:left-[50%] bottom-[0px] lg:bottom-[20px] xl:bottom-[30px] -translate-x-1/2 w-[180px] h-[180px] lg:w-[350px] lg:h-[350px] xl:w-[400px] xl:h-[400px] select-none z-0 opacity-7 pointer-events-none"
+      
       >
-        N
+        <Image src="/watermarkblack.png" alt="Watermark" fill className="object-contain" priority />
       </div>
 
-      {/* SLIDESHOW GAMBAR - DIPERBESAR (SEMAKIN KE TEPI, MEMPERKECIL RUANG KOSONG) */}
-      <div className="absolute right-[2%] lg:right-[2%] top-1/2 -translate-y-1/2 w-[96%] sm:w-[90%] md:w-[80%] lg:w-[70%] xl:w-[65%] h-auto lg:h-[92vh] aspect-[3/4] z-10 bg-gray-50 overflow-hidden shadow-2xl">
+      {/* SLIDESHOW GAMBAR - DIRAMPINGKAN (LEBAR DIPERKECIL + RASIO 1:2) */}
+      <div className="absolute right-[2%] lg:right-[4%] top-1/2 -translate-y-1/2 w-[75%] sm:w-[65%] md:w-[55%] lg:w-[45%] xl:w-[40%] h-auto lg:h-[92vh] aspect-[1/2] z-10 bg-gray-30 overflow-hidden">
         {images.map((img, index) => (
           <div
             key={`${img}-${index}`}
