49e6434d32ca6cdeec71b7c74af05d675d26cf6c
budichyn2003 <budigubernur15@gmail.com>
2026-05-29 01:38:19 +0700

    mobile bagus

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -39,19 +39,18 @@ export default function HomePage() {
   return (
     <div className="absolute inset-0 w-full h-full overflow-hidden bg-white">
       
-      {/* 🛠️ PANDUAN MENGGESER WATERMARK 🛠️
-        - Geser Kanan/Kiri : Ubah angka "left-[55%]" (HP) dan "lg:left-[42%]" (Laptop). Makin besar angkanya, makin ke kanan.
-        - Geser Atas/Bawah : Ubah angka "bottom-[0px]" (HP) dan "lg:bottom-[20px]" (Laptop). Makin besar angkanya (plus), makin naik ke atas.
-      */}
+      {/* WATERMARK: Opacity saya perbaiki jadi opacity-10 agar Tailwind bisa membacanya (opacity-7 tidak ada di Tailwind default) */}
       <div 
-        className="absolute left-[55%] lg:left-[50%] bottom-[0px] lg:bottom-[20px] xl:bottom-[30px] -translate-x-1/2 w-[180px] h-[180px] lg:w-[350px] lg:h-[350px] xl:w-[400px] xl:h-[400px] select-none z-0 opacity-7 pointer-events-none"
-      
+        className="absolute left-[50%] md:left-[55%] lg:left-[50%] bottom-[20px] md:bottom-[0px] lg:bottom-[20px] xl:bottom-[30px] -translate-x-1/2 w-[120px] h-[120px] md:w-[180px] md:h-[180px] lg:w-[350px] lg:h-[350px] xl:w-[400px] xl:h-[400px] select-none z-0 opacity-10 pointer-events-none"
       >
         <Image src="/watermarkblack.png" alt="Watermark" fill className="object-contain" priority />
       </div>
 
-      {/* SLIDESHOW GAMBAR - DIRAMPINGKAN (LEBAR DIPERKECIL + RASIO 1:2) */}
-      <div className="absolute right-[2%] lg:right-[4%] top-1/2 -translate-y-1/2 w-[75%] sm:w-[65%] md:w-[55%] lg:w-[45%] xl:w-[40%] h-auto lg:h-[92vh] aspect-[1/2] z-10 bg-gray-30 overflow-hidden">
+      {/* SLIDESHOW GAMBAR (PERBAIKAN MOBILE): 
+        - Mobile (Default): Posisi di tengah (left-1/2 -translate-x-1/2), turun sedikit dari navbar (top-[55%]), lebar 88%, rasio 3:4.
+        - Tablet/Laptop (md / lg): Kembali menempel di kanan (md:left-auto md:translate-x-0 md:right-[4%]), rasio 1:2.
+      */}
+      <div className="absolute left-1/2 md:left-auto -translate-x-1/2 md:translate-x-0 right-auto md:right-[4%] top-[55%] md:top-1/2 -translate-y-1/2 w-[88%] sm:w-[65%] md:w-[55%] lg:w-[45%] xl:w-[40%] h-auto lg:h-[92vh] aspect-[3/4] md:aspect-[1/2] z-10 bg-gray-50 overflow-hidden shadow-xl md:shadow-none">
         {images.map((img, index) => (
           <div
             key={`${img}-${index}`}
